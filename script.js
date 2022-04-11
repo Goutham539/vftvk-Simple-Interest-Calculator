0f0a121baa27f2c56b6258a1e5aad479eb9d7a94
Goutham Nerella <45123323+Goutham539@users.noreply.github.com>
2022-04-11 15:01:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var result = principal * years * rate /100;
+    var final_year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "
+    +principal+",\<br\>at an interest rate of "
+    +rate+"%\<br\>You will receive an amount of "
+    +result+",\<br\>in the year "+final_year+"\<br\>"
 
     function updateRate(){
         var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval;
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("rate_val").innerText=rateval;   
+    }
+
+    function checkAmount(){
+        var principal = document.getElementById("principal").value;
+        if(principal<=0){
+            alert("Enter a positive number greater than 0");
+            document.getElementById("principal").focus();
+        }
     }
 }
+
         
