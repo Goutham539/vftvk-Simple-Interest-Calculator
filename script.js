062f95991d06cc7c0cd5e4c6809d882171e20207
Goutham Nerella <45123323+Goutham539@users.noreply.github.com>
2022-04-12 08:30:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function compute()
 {
+    // Collecting variables from user
+        
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var result = principal * years * rate /100;
     var final_year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "
-    +principal+",\<br\>at an interest rate of "
-    +rate+"%\<br\>You will receive an amount of "
-    +result+",\<br\>in the year "+final_year+"\<br\>"
+    // Showing result in form of a paragraph 
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "
+    +rate+"%\<br\>You will receive an amount of "+result+",\<br\>in the year "+final_year+"\<br\>"
 }
+    
+    // Function updateRate is used to show the rate selected by slider dynamically.
     function updateRate(){
         var rateval = document.getElementById("rate").value;
         document.getElementById("rate_val").innerText=rateval;   
     }
 
+    //CheckAmount function checks if the principal amount entered by user is greater than 0 or not.
     function checkAmount(){
         var principal = document.getElementById("principal").value;
         if(principal<=0){
